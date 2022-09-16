41c201f4953827f17290c9aa44be4abba26eada3
Thiago <ps-thiago@hotmail.com>
2022-09-16 18:51:39 -0300

    Feat: prisma

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,13 @@ app.get('/games', (request, response) => {
   return response.json([]);
 })
 
-app.post('/games', (request, response) => {
-  return response.json([]);
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
 })
 
-app.get('/ads', (request, response) => {
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id;
+
   return response.json([
     { id: 1, name: 'Anúncio 1' },
     { id: 2, name: 'Anúncio 2' },
@@ -20,4 +22,9 @@ app.get('/ads', (request, response) => {
   ])
 })
 
+app.get('/ads/:id/discord', (request, response) => {
+  // const gameId = request.params.id;
+
+  return response.json([])
+})
 app.listen(3333)
